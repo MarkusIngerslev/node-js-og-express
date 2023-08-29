637f0560da3d54819d61ce8d8ce5074b925a17bf
MarkusIngerslev <markusolsen2004@gmail.com>
2023-08-29 20:24:39 +0200

    tilføjet cors og express dependencies, samt prøvet at få noget vist på hjemmesiden

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,24 @@
+import express from "express";
 import fs from "fs/promises";
+
+// ===== const til at lave database ===== //
+const app = express();
+const port = 3333;
+
+// ===== Lytter til at hvilken port node.js er på ===== //
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
+
+// ===== ROUTE "/" - GET ===== //
+app.get("/", (req, res) => {
+    res.send(`Main page for node.js 👍`);
+});
+
+// ===== ROUTE show Artist ===== //
+app.get("/users", async (req, res) => {
+    const data = await fs.readFile("./data/artists.json");
+    const artist = JSON.parse(data);
+
+    res.json(artist);
+});
