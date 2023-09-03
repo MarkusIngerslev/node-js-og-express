672d2b7fb2879fec74ce0f29d1682cad0cee732a
MarkusIngerslev <markusolsen2004@gmail.com>
2023-09-03 17:28:07 +0200

    Tilføjet ekstra fil til at håndtere hjælpe funktioner

---
+++ frontend/js/helpers.js
@@ -0,0 +1,23 @@
+// funktion til at fjerne mulige mellemrum og ændre til stort forbogstav i genres og labels
+function trimAndCapitalize(word) {
+    return word.trim().replace(/\b\w/g, (c) => c.toUpperCase());
+}
+
+// funktion til at kunne vælge mellem forskellige genres
+function updateDatalistGenres(artistObject) {
+    const datalist = document.querySelector("#genre-list");
+    const uniqueGenres = new Set();
+
+    artistObject.forEach((artist) => {
+        artist.genres.forEach((genre) => {
+            if (!uniqueGenres.has(genre)) {
+                uniqueGenres.add(genre);
+                const option = document.createElement("option");
+                option.value = genre;
+                datalist.appendChild(option);
+            }
+        });
+    });
+}
+
+export { trimAndCapitalize, updateDatalistGenres };
